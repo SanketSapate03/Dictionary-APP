ca528168aeafd4cb93a035e2e91a315cdc16a2f6
SanketSapate03 <ssanket0333@gmail.com>
2024-04-30 22:32:49 +0530

    first commit

---
+++ app/src/main/java/com/example/dictionaryapp/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.dictionaryapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Set a delay for how long the splash screen will be shown
+        Handler().postDelayed({
+            // Start your main activity here
+            startActivity(Intent(this, MainActivity::class.java))
+            // Finish this activity
+            finish()
+        }, 3000)
+    }
+}
